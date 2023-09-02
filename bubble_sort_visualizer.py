eb4a234b7fb49b8133d9bc95e7cb54ac3a5e51c9
Charlie Nourrcier <cnourrcier@hotmail.com>
2023-09-01 22:32:01 -0500

    adjusted parameters for easier visualization

--- bubble_sort_visualizer.py
+++ bubble_sort_visualizer.py
@@ -3,13 +3,13 @@ import random
 import time
 
 # Generate random data
-data = [random.randint(1, 100) for _ in range(50)]
+data = [random.randint(1, 100) for _ in range(10)]
 
 # Visualize the data
 def visualize(data):
     plt.bar(range(len(data)), data)
     plt.show(block=False) # block=False means to not block code execution
-    plt.pause(0.1)        # Pause to allow time for visualization
+    plt.pause(0.5)        # Pause to allow time for visualization
     plt.close()           # Close the graph window after pausing
 
 def bubble_sort_visualized(arr):
@@ -18,7 +18,7 @@ def bubble_sort_visualized(arr):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-        visualize(arr) # Call the visualize function after each pass
+                visualize(arr) # Call the visualize function after each pass
 
 # Call the sorting function
 bubble_sort_visualized(data)
